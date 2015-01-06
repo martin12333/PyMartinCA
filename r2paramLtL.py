1919fc789a66f9b029bbac548dfe26531391cc30
Martin Novy <martin1234@ufoni.cz>
2015-01-06 17:22:27 +0100

    afade

--- r2paramLtL.py
+++ r2paramLtL.py
@@ -1,3 +1,8 @@
+# beta ver
+
+#NOTE: the showing of images does NOT currently work when running r2paramLtL.py from SPYDER v2.2
+
+
 # evolved from
 #http://nbviewer.ipython.org/github/martin12333/teaching-simso/blob/master/52-largerthanlife.ipynb
 #https://github.com/thearn/game-of-life/blob/master/conway.py
@@ -9,10 +14,15 @@ import matplotlib.pyplot as plt
 
 
 diam =5 
-xlo= 11 
-xhi=12 
-ylo= 9 
-yhi= 10 
+#xlo= 7
+#xhi=11
+#ylo=  10
+#yhi= 11
+xlo= 10
+xhi=11
+ylo=  10
+yhi= 10
+
 
 #diam =3   
 #xlo= 2 
@@ -20,7 +30,7 @@ yhi= 10
 #ylo=3 
 #yhi=5  
 
-
+#h=180
 #h=730 
 h=360
 #w=h*2
@@ -49,17 +59,36 @@ slo = 7  #3
 shi=yy
 
 
-niter = 20000    
+niter = 300    
 sli=0.1
-skipy=1 #2
+skipy=4 #2
 
+q = 0.1
+qqq=8
+#q = 1. / 2.
+afade = 0.1 * a
+#averyold = 0*a
 
 
 plt.ion()
+#plt.ioff()
+
 matplotlib.rc("image",cmap="gray")
+#matplotlib.rc("image",cmap="hot")
+##matplotlib.rc("image",cmap="RdYlGn")
+##matplotlib.rc("image",cmap="BrBG")
+#matplotlib.rc("image",cmap="copper")
+#matplotlib.rc("image",cmap="afmhot")
+#matplotlib.rc("image",cmap="autumn")
+#matplotlib.rc("image",cmap="summer")
 matplotlib.rc("image",interpolation="nearest")
+##np.dstack()
+##np.concatenate()
+###(tup, axis=2)
+####plt.matshow())
 #plt.figure()
-img_plot = plt.imshow(a, interpolation="nearest", cmap = plt.cm.gray)
+img_plot = plt.imshow(a, interpolation="nearest")
+#, cmap = plt.cm.gray)
 plt.show(block=False)
 
 OR = logical_or
@@ -75,10 +104,24 @@ for i in range(niter):
     ##drawnow()
     ###matplotlib.pyplot.show()
     if (i % skipy)==0:
-        img_plot.set_data(a)
+        #asho = (2 * a + averyold )/3
+        #imagesc(abs(y-yveryold)')
+        #%imagesc((y-yveryold)')
+        #asho = np.maximum( a , averyold /3 )
+        #asho = np.maximum( a , averyold /3. )
+        #asho = np.maximum( a , afade/3. )
+        asho = np.maximum( a , qqq*afade )
+        #asho = afade 
+        afade = q*a + (1-q)*afade
+        #averyold = a
+        img_plot.set_data(asho)
+        ###imshow(asho)
+        ##show()
         plt.draw()
         time.sleep(sli)
+        print i
 
 
 ##debug print shape(blo)
 ##debug numpy.size(yy,0)
+##debug typename(blo)
