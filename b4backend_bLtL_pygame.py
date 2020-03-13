83ce5e5574e3c111a111d56b015958d26023b806
Martin Novy <martin12333@users.noreply.github.com>
2020-03-13 15:46:36 +0100

    hist

--- b4backend_bLtL_pygame.py
+++ b4backend_bLtL_pygame.py
@@ -14,52 +14,14 @@ OR = logical_or
 AND = logical_and
 
 for i in range(niter):
-    if False: #bltl :
-        n = floor(0.5+diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
-        birth = AND(  n>=blo,  AND( n<=bhi, logical_not(a) )  )
-        survi = AND(  n>=slo,  AND( n<=shi, a )  )
-        a = 1.0* OR( birth , survi )  
-    elif False: #r62
-        #nbig=floor(0.5  + 2*diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
-        nbig=( 2*diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
-        #nsmall = floor(0.5 + alpha03* d03*d03* ( filters.uniform_filter(a,d03,mode='wrap')  ))
-        nsmall = ( alpha03* d03*d03* ( filters.uniform_filter(a,d03,mode='wrap')  ))
-        n=nbig - nsmall
-        ##n = floor(0.5 +  diam*diam* ( - a /9.0 +  2*filters.uniform_filter(a,diam,mode='wrap')  ))
-        #n = floor(0.5 - a + 2*diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
-        a = 1.0* AND( n>=xx , n<=yy )  
-    elif False: #r69 l1x
-        n69=diam*diam*filters.uniform_filter(a,diam,mode='wrap')
-        n=n69
-        a70=(a>=0.5)
-        birth = AND(  n>=blo,  AND( n<=bhi, logical_not(a70) )  )
-        survi = AND(  n>=slo,  AND( n<=shi, a70 )  )
-        b70 = 1.0* OR( birth , survi )  
-        #
-        a = beta70*b70 + nbeta70*a
-    elif False: #r70
-        norm70=diam*diam/((1-alf70)*diam*diam +  (alf70)*di70*di70  )
-        ###print norm70
-        n60 =  (1-alf70)*diam*diam*filters.uniform_filter(a,diam,mode='wrap')  
-        n70 =  (alf70)*di70*di70*filters.uniform_filter(a,di70,mode='wrap')  
-        n=(n60+n70) * norm70
-        a70=(a>=0.5)
-        birth = AND(  n>=blo,  AND( n<=bhi, logical_not(a70) )  )
-        survi = AND(  n>=slo,  AND( n<=shi, a70 )  )
-        b70 = 1.0* OR( birth , survi )  
-        #
-        a = beta70*b70 + nbeta70*a
-    elif True: #r75 l2x
-        n751 = (diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
-        n752 = (diam*diam*filters.uniform_filter(n751,diam,mode='wrap')  )
-        #n753 = (diam*diam*filters.uniform_filter(n752,diam,mode='wrap')  )
-        #n754 = (diam*diam*filters.uniform_filter(n753,diam,mode='wrap')  )
-        n=n752
-        a70=(a>=0.5)
-        birth = AND(  n>=blo,  AND( n<=bhi, logical_not(a70) )  )
-        survi = AND(  n>=slo,  AND( n<=shi, a70 )  )
-        b70 = 1.0* OR( birth , survi )  
-        a = beta70*b70 + nbeta70*a
+    n41 = floor(0.5+diam*diam*filters.uniform_filter(a,diam,mode='wrap')  )
+    n42 = floor(0.5+diam*diam*filters.uniform_filter(n41,diam,mode='wrap')  )
+    n=n42
+    birth = AND(  n>=blo,  AND( n<=bhi, logical_not(a) )  )
+    survi = AND(  n>=slo,  AND( n<=shi, a )  )
+    a = 1.0* OR( birth , survi )  
+    ##n753 = (diam*diam*filters.uniform_filter(n752,diam,mode='wrap')  )
+    ##n754 = (diam*diam*filters.uniform_filter(n753,diam,mode='wrap')  )
     #imshow(a)  
     ##matshow()
     ##drawnow()
---
+++ l41param_map_LtL.py
@@ -0,0 +1,91 @@
+# originally evolved from
+#http://nbviewer.ipython.org/github/martin12333/teaching-simso/blob/master/52-largerthanlife.ipynb
+#https://github.com/thearn/game-of-life/blob/master/conway.py
+
+from pylab import *
+
+#=============================
+#a standard rule notation
+#https://www.conwaylife.com/wiki/Larger_than_Life
+#Rr,Cc,M1,Ssmin..smax,Bbmin..bmax,Nn
+#Rr specifies the range (r is from 1 to 500 in Golly and LifeViewer; 1 to 10 in MCell).
+#Cc specifies the number of states (c is from 0 to 255 in Golly, LifeViewer and MCell[note 1])
+#Mm specifies if the middle cell is included in the neighborhood count (m is 0 or 1).
+#Ssmin..smax specifies the count limits for a state 1 cell to survive.
+#Bbmin..bmax specifies the count limits for a dead cell to become a birth.
+#Nn specifies the extended neighborhood type (n is M for Moore or N for von Neumann. Golly and LifeViewer also support C for Circular neighborhood)
+
+#in legacy  source code, by Martin Novy
+#diam == 2*r + 1
+#bfade, q, qqq are related to Cc
+#M1 or weighted
+#smin==slo
+#smax==shi
+#bmin==blo
+#bmax==bhi
+#NM or weighted
+
+
+
+#=============================
+blo=24 ##23.5 ##2.60 #70 #60 #50 ##6.5 ###7*(beta70 - d691) ##2.98 #3*beta70 #xx #3
+slo=31 ##30.5 ##3.35 #25 #15 #01 #2.9 #8 #7  #20 #30 #40 #50 ##6.5 ###7*(beta70 - d691) #8  ##2.98 #3*beta70 #yy - 1 #slo = 3
+
+#diam =5 
+#xlo= 7 #9 ##7
+#xhi=7.08 #11 #2 #5 #8 #9 #12 ##11
+#ylo= 11.03 #01 #10.99 #95 #8 #10.5 #10 #9 ##8 #10
+#yhi= 11.08 #05 #2 #5 #12 #13 ##12
+
+diam =3
+xlo=23 ##22.5 ##2.5 #2.9 #2.8 #2.5 #2
+xhi=45 ##45.5   ##5 #3.6 #4.1 #4.2 #4.5 #5 #10
+ylo=28 ##27.5 ##3 # 4.4 #4.2  #3 #3.9 #3.8
+yhi=54 ##54.5 ##6 #4.3 #4.8  #6 #5.1 #5.2 #10
+
+#=============================
+h=950 #1040 #h=180 #h=730 #h=360 #w=h*2
+w=h
+
+p=0.25 #0.16
+a = 1.0*(rand( h, w )<p)    
+
+x11 = floor(linspace(xlo, xhi + 0.999, w))
+y11 = floor(linspace(yhi + 0.999 , ylo, h))
+##x11 = (linspace(xlo, xhi , w))
+##y11 = (linspace(yhi  , ylo, h))
+xx, yy = meshgrid(x11, y11)
+ 
+bhi=xx
+shi=yy
+
+niter = 3000   
+sli=0 #0.1 #0 #0.01
+skipy=5 #1 #37 #5 #2
+
+bfade =  True  # False
+q = 0.3 #0.1 #q = 1. / 2.
+qqq=4 #8 #qqq=1.8
+afade = 0.1 * a
+#averyold = 0*a
+
+
+
+#matplotlib.rc("image",cmap="gray")
+#matplotlib.rc("image",cmap="hot")
+##matplotlib.rc("image",cmap="RdYlGn")
+##matplotlib.rc("image",cmap="BrBG")
+#matplotlib.rc("image",cmap="copper")
+#matplotlib.rc("image",cmap="afmhot")
+#matplotlib.rc("image",cmap="autumn")
+#matplotlib.rc("image",cmap="summer")
+
+#matplotlib.rc("image",interpolation="nearest")
+
+###np.dstack()
+###np.concatenate()
+####(tup, axis=2)
+
+
+#execfile('backend_LtL_imshow.py')
+execfile('b4backend_bLtL_pygame.py')
