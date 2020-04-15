739ed461bd548b7c7f083d0e994d3a9c3073d595
Martin Novy <martin12333@users.noreply.github.com>
2020-04-15 15:01:07 +0200

    plot

--- pop-plot/backend_LtL_imshow.py
+++ pop-plot/backend_LtL_imshow.py
@@ -39,51 +39,70 @@ OR = logical_or
 AND = logical_and
 
 
-#ploty=zeros( (niter,1) )
-ploty=zeros( (niter,1) )
+plotysum=zeros( (niter,1) )
 
-for i in range(niter):
+for j in xrange(ns):
 
-    n41 =   diam*diam*filters.uniform_filter(0.0+a,diam )   ##,mode='wrap')  )  
-    n=n41.round()   
-    birth = AND(  n>=bmin,  AND( n<=bmax, logical_not(a) )  )
-    survi = AND(  n>=smin,  AND( n<=smax, a )  )
-    a =  OR( birth , survi )  
+    p=0.5#0.3#0.5 #0.25 #0.16
+    a = zeros( ( h, w ), dtype=bool) 
+    
+    a[ h0 : h0+h2 , w0 : w0+w2 ] = (rand( h2, w2 )<p) 
 
-    as51=(0.0+a).sum()
-    ploty[i]=as51
+    
+    afade = 0.1 * a
+    averyold = a 
 
-    if (i % step1)==0:
 
-        if bfade:
-            afade = q*a + (1-q)*afade
-            asho=afade
-        else:
-            asho=0+a
+    #plotx=zeros( (niter,1) )
+    ploty=zeros( (niter,1) )
+
+    for i in xrange(niter):
+
+        n41 =   diam*diam*filters.uniform_filter(0.0+a,diam )   ##,mode='wrap')  )  
+        n=n41.round()   
+        birth = AND(  n>=bmin,  AND( n<=bmax, logical_not(a) )  )
+        survi = AND(  n>=smin,  AND( n<=smax, a )  )
+        a =  OR( birth , survi )  
+
+        as51=(0.0+a).sum()
+        ploty[i]=as51
+
+        if (i % step1)==0:
+
+            if bfade:
+                afade = q*a + (1-q)*afade
+                asho=afade
+            else:
+                asho=0+a
+                
+            ada51= logical_xor(a, averyold)  
+            averyold =a 
             
-        ada51= logical_xor(a, averyold)  
-        averyold =a 
-        
-        ##adaimage = transpose( ( ( asho)))
-        ##b = array(255*clip(adaimage,0,1),'B')
+            ##adaimage = transpose( ( ( asho)))
+            ##b = array(255*clip(adaimage,0,1),'B')
 
-        ###imshow(asho)
-        ##show()
+            ###imshow(asho)
+            #show()
 
 
-        #img_plot.set_data(asho)
-        #plt.draw()
+            #img_plot.set_data(asho)
+            #plt.draw()
 
 
-        ##time.sleep(sleep1)
-        
+            ##time.sleep(sleep1)
+            
 
-        if (i % step2)==0:
-            am51=(0.0+a).mean()
-            sam51='{:.3f}'.format(am51)
-            dam51=ada51.mean()
-            sdam51='{:.3f}'.format(dam51)
-            print( i, sam51, sdam51)
+            if (i % step2)==0:
+                am51=(0.0+a).mean()
+                sam51='{:.5f}'.format(am51)
+                dam51=ada51.mean()
+                sdam51='{:.5f}'.format(dam51)
+                print( i, sam51, sdam51)
 
 
-plot(ploty)
+    plot(ploty,'x')
+    plotysum += ploty
+    
+    
+    
+    
