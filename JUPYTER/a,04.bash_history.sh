97e9dd3908b7bd78ff6a8402f57874da969d790b
Martin Novy <martin12333@users.noreply.github.com>
2020-06-29 15:32:04 +0200

    forum

---
+++ JUPYTER/a,04.bash_history.sh
@@ -0,0 +1,523 @@
+    1  history 
+    2  sudo apt-get install sshd
+    3  sudo apt-get install openssh-sftp-server 
+    4  ll
+    5  history 
+    6  ps -u martin |grep fire
+    7  ps -ef |grep fire
+    8  ps -e |grep fire
+    9  ps -f |grep fire
+   10  ps -ef |grep fire
+   11  top
+   12  ps -ef |grep fire
+   13  firefox 
+   14  cd Downloads/
+   15  ll
+   16  sudo -i dpkg --install code_1.36.1-1562627527_amd64.deb 
+   17  pwd
+   18  sudo -i dpkg --install /home/martin/Downloads/code_1.36.1-1562627527_amd64.deb 
+   19  history 
+   20  history >vd182-2223new,j7o.history.sh
+   21  history -a
+   22  history >vd182-2223new,j7o.history.sh
+   23  cd
+   24  history >vd182-2223new,j7o.history.sh
+   25  cat vd182-2223new,j7o.history.sh 
+   26  /usr/bin/sudo -i /bin/nano --backup /etc/gdm3/custom.conf
+   27  history 
+   28  cd Downloads/
+   29  sudo -i dpkg --install /home/martin/Downloads/Brackets.Release.1.14.64-bit.deb 
+   30  history 
+   31  sudo apt-get install gconf-service
+   32  sudo apt-get --fix-broken install
+   33  xdg-icon-resource --help
+   34  history 
+   35  history >vd182-2223new,j7q.history.sh
+   36  cat vd182-2223new,j7q.history.sh 
+   37  history -a
+   38  history 
+   39  history -a
+   40  history 
+   41  history -a
+   42  history 
+   43  history -a
+   44  history 
+   45  history -a
+   46  set|less
+   47  history 
+   48  set
+   49  set | grep -i D
+   50  set | grep D
+   51  set | grep D|less
+   52  DISPLAY=:0
+   53  export DISPLAY
+   54  history -a
+   55  history 
+   56  history -a
+   57  history 
+   58  brackets --help
+   59  brackets --debug
+   60  brackets --help
+   61  top
+   62  brackets 
+   63  top
+   64  history 
+   65  ps -ef |grep -i bracke
+   66  history 
+   67  history -a
+   68  history 
+   69  history -a
+   70  history 
+   71  sudo apt-get install xfce4
+   72  history 
+   73  history -a
+   74  history 
+   75  history -a
+   76  cat /proc/cpuinfo 
+   77  brackets 
+   78  history 
+   79  snap --help
+   80  fish
+   81  mc
+   82  sudo apt install fish
+   83  snap login
+   84  sudo snap login
+   85  snap install node --classic --channel=10
+   86  history 
+   87  history -a
+   88  history 
+   89  node --version
+   90  npm --version
+   91  yarn --version
+   92  history -a
+   93  fish
+   94  sudo apt install git
+   95  history 
+   96  history -a
+   97  fish
+   98  #git clone --recursive https://github.com/plasma-umass/browsix
+   99  fish
+  100  sudo apt install make
+  101  fish
+  102  sudo apt install mc
+  103  fish
+  104  sudo apt install make-guile
+  105  history -a
+  106  fish
+  107  sudo apt install zsh
+  108  history -a
+  109  zsh
+  110  history -a
+  111  zsh
+  112  mc
+  113  zsh
+  114  history -a
+  115  top
+  116  vmstat 5
+  117  top --help
+  118  man top
+  119  df -m
+  120  cat test.txt| node uniq.js
+  121  nl test.txt| node uniq.js
+  122  wc test.txt| node uniq.js
+  123  wc test.txt| node wc.js
+  124  sort  test.txt| node uniq.js
+  125  sort  test.txt| node cut.js
+  126  wc test.txt| node od.js
+  127  cat test.txt| node od.js
+  128  cat test.txt| node tr.js
+  129  cat test.txt| node cp.js
+  130  cat test.txt| node sort.js
+  131  cat test.txt| node sort.js|node uniq.js
+  132  cat test.txt| node sort.js|cat|node uniq.js
+  133  cat test.txt| node sort.js|cat
+  134  cat test.txt| node sort.js|wc
+  135  cat test.txt| node sort.js|tee martin123|node uniq.js
+  136  cat test.txt| node sort.js|tee martin123
+  137  cat  martin123|node uniq.js
+  138  history 
+  139  zsh
+  140  history 
+  141  zsh
+  142  d8 --help
+  143  ./d8 --help
+  144  mc
+  145  df -m
+  146  du -sm /*
+  147  du -sm /* 2>2
+  148  du -sm /snap/* 2>2
+  149  du -am /swapfile 2>2
+  150  mc
+  151  top
+  152  du -am /usr/* 2>2
+  153  zsh
+  154  history 
+  155  zsh
+  156  dmesg
+  157  journalctl -b
+  158  journalctl -b|grep tea
+  159  mc
+  160  top
+  161  sudo apt install qemu-system-x86
+  162  sudo apt-get update
+  163  sudo apt install qemu-system-x86
+  164  cd rh-9/
+  165  bunzip2 --help
+  166  bunzip2 --keep rh-9-shrike.img.bz2 
+  167  qemu-system-i386 rh-9-shrike.img
+  168  cd rh-9/
+  169  qemu-system-i386 rh-9-shrike.img
+  170  ll
+  171  mc
+  172  cd Downloads/
+  173  sudo dpkg -i nomachine_6.9.2_1_amd64.deb
+  174  history -a
+  175  mc
+  176  sudo ls -la /etc/sudoers.d/
+  177  echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee --append /etc/sudoers.d/$USER
+  178  sudo ls -la /etc/sudoers.d/
+  179  history -a
+  180  mc
+  181  history 
+  182  sudo ls -la /etc/sudoers.d/
+  183  mc
+  184  git init
+  185  git checkout -b vd182
+  186  git remote add origin https://github.com/actes/branched--newbie-attempts--dotfiles
+  187  git add --verbose --dry-run 
+  188  echo $(cat 
+  189  ./git\ ls-files )
+  190  ll git\ ls-files 
+  191  echo $(cat ./git\ ls-files )
+  192  git add --verbose --dry-run $(cat ./git\ ls-files )
+  193  git add --verbose  $(cat ./git\ ls-files )
+  194  git show
+  195  git status 
+  196  git commit  -a -m 'hist' ;  
+  197  git push -u origin vd182
+  198  git add .config/fish/fish_history
+  199  history 
+  200  git show
+  201  history 
+  202  git status 
+  203  git commit  -a -m 'hist' ;  
+  204  git show
+  205  git status 
+  206  git push -u origin vd182
+  207  git add .gitconfig
+  208  history 
+  209  echo git commit  -a -m 'hist' ;git push -u origin vd182
+  210  git commit  -a -m 'hist' ;  
+  211  git push -u origin vd182
+  212  history -a #----
+  213  git commit  -a -m 'hist' ;git push -u origin vd182
+  214  /usr/bin/sudo -i  /usr/bin/apt-get install    --download-only   atari800  mess mame-extra mame mame-tools   gnome-video-arcade  
+  215  /usr/bin/sudo -i  /usr/bin/apt-get install    --download-only   atari800   mame-extra mame mame-tools   gnome-video-arcade  
+  216  /usr/bin/sudo -i  /usr/bin/apt-get install     atari800   mame-extra mame mame-tools   gnome-video-arcade  
+  217  cd atari-xf25/
+  218  ll
+  219  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  220  atari800 --h
+  221  atari800 --help
+  222  atari800  -mouse joy  Mario_Bros._1983_Atari_US_t_5200.atr 
+  223  atari800 --help|grep -i key
+  224  atari800 --help|grep -i joy
+  225  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  226  atari800 --help|grep -i mou
+  227  atari800  -mouse off  Mario_Bros._1983_Atari_US_t_5200.atr 
+  228  mame --help
+  229  mame -h
+  230  mame 
+  231  mame -showusage
+  232  mame -h
+  233  mame -listmedia
+  234  mame -listmedia|grep -i atar
+  235  mame -h
+  236  mc
+  237  cd atari-xf25/
+  238  history
+  239  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  240  history -a #----
+  241  cd atari-xf25/
+  242  mame -verifyroms a800xlp
+  243  mame -verifyroms a800xl
+  244  mame -verifyroms 
+  245  mame a2600
+  246  ll
+  247  la
+  248  la history/
+  249  ll ..
+  250  df -m
+  251  df -m >../df-m
+  252  mame a800
+  253  la
+  254  la ..
+  255  cat ../.atari800.cfg 
+  256  mc
+  257  history
+  258  history -a
+  259  mame a2600
+  260  mame a800
+  261  mame a800xl
+  262  mame a800xlp
+  263  mame a5200
+  264  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  265  history -a #----
+  266  cp -i ../.atari800.cfg ../2.atari800.cfg 
+  267  history -a #----
+  268  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  269  atari800 ATARIBAS.ROM 
+  270  atari800 -basic
+  271  pwd
+  272  history -a
+  273  ll
+  274  pwd
+  275  history -a #----
+  276  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  277  ll
+  278  ll ..
+  279  ls -latr ..
+  280  ls -latr 
+  281  less ../.atari800.cfg 
+  282  atari800 Mario_Bros._1983_Atari_US_t_5200.atr 
+  283  ls -latr ..
+  284  cd ..
+  285  cd atari-xf25/
+  286  diff ../.atari800.cfg ../2.atari800.cfg 
+  287  cp -i ../.atari800.cfg ../3.atari800.cfg 
+  288  history -a #----
+  289  mc
+  290  /usr/bin/sudo -i  /usr/bin/apt-get install   ipython python-scipy python-pygame
+  291  sudo apt-get update
+  292  /usr/bin/sudo -i  /usr/bin/apt-get install   ipython python-scipy python-pygame
+  293  export DISPLAY=:0
+  294  history -a #----
+  295  cd grdr20/PyMartinCA/beta/
+  296  history -a #----
+  297  ipython 
+  298  ipython --pylab
+  299  /usr/bin/sudo -i  /usr/bin/apt-get install   ipython python-scipy python-pygame python-matplotlib
+  300  ipython --pylab
+  301  echo $DISPLAY 
+  302  cd grdr20/PyMartinCA/beta/
+  303  ipython 
+  304  cd ..
+  305  cd PyMartinCA/
+  306  ipython 
+  307  mc
+  308  ipython 
+  309  ll
+  310  ipython 
+  311  cd ..
+  312  cd beta/
+  313  cd ..
+  314  mc
+  315  cd experim2015/
+  316  ipython 
+  317  cd ..
+  318  cd beta/
+  319  ipython 
+  320  mc
+  321  cd ..
+  322  cd experim2015/
+  323  ipython 
+  324  history -a #----
+  325  ipython 
+  326  history -a #----
+  327  mc
+  328  /usr/bin/sudo -i  /usr/bin/apt-get install   gimp
+  329  history -a #----
+  330  mc
+  331  export DISPLAY=:0
+  332  ~/Downloads/golly-3.3-gtk-64bit/golly &
+  333  history -a #----
+  334  /usr/bin/sudo -i  /usr/bin/apt-get install   libpng-dev 
+  335  ~/Downloads/golly-3.3-gtk-64bit/golly &
+  336  history -a #----
+  337  ipython 
+  338  history |grep grep
+  339  export DISPLAY=:0
+  340  cd experim2015/
+  341  ll
+  342  cd grdr20/PyMartinCA/experim2015/
+  343  ipython 
+  344  history -a #----
+  345  ipython 
+  346  cd ..
+  347  cd beta/
+  348  history -a #----
+  349  ipython 
+  350  cd ex5/
+  351  ipython 
+  352  git commit  -a -m 'hist' ;git push -u origin vd182
+  353  cd grdr20/PyMartinCA/beta/ex5/
+  354  ipython 
+  355  export DISPLAY=:0
+  356  history -a #----
+  357  ipython 
+  358  history -a #----
+  359  /usr/bin/sudo -i  apt-get install  openjdk-11-jre
+  360  /usr/bin/sudo -i  apt-get install  openjdk-8-jre
+  361  export DISPLAY=:0
+  362  history -a #----
+  363  cd Downloads/sqc/
+  364  ll
+  365  run
+  366  ./run
+  367  mc
+  368  chmod u+x *.jar
+  369  ./SquareCell.jar 
+  370  java -jar SquareCell.jar 
+  371  history -a #----
+  372  cd grdr20/PyMartinCA/beta/ex5/
+  373  ipython 
+  374  export DISPLAY=:0
+  375  history -a #----
+  376  export DISPLAY=:0
+  377  cd grdr20/PyMartinCA/beta/ex5/
+  378  ipython 
+  379  ipython 
+  380  cd Downloads/sqc/
+  381  cd
+  382  cd Downloads/sqc/
+  383  java -jar SquareCell.jar 
+  384  cd
+  385  cd grdr20/PyMartinCA/beta/ex5/
+  386  cd ..
+  387  cd xgauss/
+  388  ipython 
+  389  mc
+  390  cd
+  391  cd grdr20/PyMartinCA/beta/xgauss9/
+  392  ipython 
+  393  cd
+  394  history -a #----
+  395  mc
+  396  cd grdr20/PyMartinCA/beta/xgauss9/
+  397  ipython 
+  398  cd
+  399  cd grdr20/PyMartinCA/beta/ex5/
+  400  ipython 
+  401  export DISPLAY=:0
+  402  ps -ef |grep -i ipy
+  403  history -a #----
+  404  history
+  405  cd grdr20/PyMartinCA/beta/ex5/
+  406  ipython 
+  407  cat r2b7t*.py
+  408  ipython 
+  409  cd
+  410  mc
+  411  history -a #----
+  412  mc
+  413  cd grdr20/PyMartinCA/beta/ex5/
+  414  ipython 
+  415  2to3-2.7 --help
+  416  2to3-2.7 --list
+  417  2to3-2.7 --help
+  418  2to3 --verbo backend_bLtL_pygame.py 
+  419  2to3-2.7 --verbo backend_bLtL_pygame.py 
+  420  2to3-2.7  backend_bLtL_pygame.py 
+  421  2to3-2.7 --help
+  422  2to3-2.7 --prin  backend_bLtL_pygame.py 
+  423  history -a #----
+  424  history
+  425  cat backend_bLtL_pygame.py 
+  426  ipython 
+  427  dpkg -l
+  428  dpkg -l|grep ipyth
+  429  dpkg -l|grep jupy
+  430  apt search jupyter
+  431  apt search jupyter|less
+  432  apt search jupyterlab|less
+  433  apt search ipython|less
+  434  sudo apt-get update
+  435  sudo apt install jupyter-notebook python3-matplotlib
+  436  history -a #----
+  437  cd grdr20/PyMartinCA/beta/
+  438  jupyter --debug
+  439  jupyter notebook --debug
+  440  cd grdr20/PyMartinCA/beta/
+  441  jupyter notebook --debug
+  442  history -a #----
+  443  export DISPLAY=:0
+  444  history -a #----
+  445  cd ex5/
+  446  ipython 
+  447  cd ..
+  448  mc
+  449  cd LtL--a-as-bool8--204/
+  450  history -a #----
+  451  ipython3
+  452  ipython
+  453  jupyter console --debug
+  454  jupyter console 
+  455  ipython
+  456  python3
+  457  mc
+  458  cd grdr20/PyMartinCA/beta/pop-plot/
+  459  history -a #----
+  460  ipython
+  461  export DISPLAY=:0
+  462  ipython
+  463  mc
+  464  ipython
+  465  mc
+  466  cd
+  467  cd grdr20/PyMartinCA/beta/hrot--bmax22/
+  468  history -a #----
+  469  cd grdr20/PyMartinCA/beta/
+  470  cd LtL--a-as-bool8--204/
+  471  export DISPLAY=:0
+  472  ipython
+  473  history -a #----
+  474  jupyter notebook --debug
+  475  cd grdr20/PyMartinCA/beta/
+  476  jupyter notebook --debug
+  477  sudo apt install jupyter-notebook python3-scipy
+  478  history -a #----
+  479  mc
+  480  cd grdr20/PyMartinCA/beta/
+  481  mc
+  482  man jupyter
+  483  man jupyter-notebook 
+  484  man jupyter-serverextension 
+  485  jupyter notebook --generate-config
+  486  mc
+  487  jupyter notebook --generate-config --help
+  488  jupyter notebook --generate-config --help|less
+  489  jupyter notebook --generate-config
+  490  ls -la /home/martin/.jupyter/
+  491  ls -la /home/martin/
+  492  ls -lad /home/martin/.jupyter/
+  493  chown martin:martin  /home/martin/.jupyter/
+  494  sudo chown martin:martin  /home/martin/.jupyter/
+  495  history -a #----
+  496  ls -lad /home/martin/.jupyter/
+  497  jupyter notebook --generate-config
+  498  mc
+  499  ipython
+  500  ls -la /etc/*rc
+  501  less /etc/matplotlibrc
+  502  mc
+  503  find / -name '*rc' 2>2
+  504  find / -name '*rc' 2>2|grep pyt
+  505  mc
+  506  cd /usr/lib
+  507  mc
+  508  history -a #----
+  509  jupyter notebook --debug
+  510  history -a #----
+  511  history -a ; history >~/Dropbox/14.04/a,04.bash_history.sh
+  512  history -a ; history >a,04.bash_history.sh
+  513  cd grdr20/PyMartinCA/beta/
+  514  history -a ; history >a,04.bash_history.sh
+  515  history -a #----
+  516  history -a ; history >a,04.bash_history.sh
+  517  mc
+  518  ipython
+  519  cd grdr20/PyMartinCA/beta/
+  520  ipython
+  521  cd
+  522  cd grdr20/PyMartinCA/beta/JUPYTER/
+  523  history -a ; history >a,04.bash_history.sh
