1209be07cea0bcdd935c33b579dd0ac13c6330b8
Martin Novy <martin12333@users.noreply.github.com>
2020-08-22 16:55:21 +0200

    license

---
+++ rle/kjgfhkf.sh
@@ -0,0 +1,31 @@
+GIT-bk04/pymartinca-beta.git
+
+
+
+GIT-bk04/pymartinca-beta.git
+
+mkdir --parents $HOME/GIT-bk04/pymartinca-beta.git
+
+
+git init --separate-git-dir $HOME/GIT-bk04/pymartinca-beta.git
+git init --separate-git-dir $HOME/GIT-bk04/pymartinca-beta.git
+
+
+/mnt/bk04/w/GIT-bk04/pymartinca-beta.git/
+   when: 1584442260
+   paths:
+    - /mnt/bk04/w/GIT-bk04/pymartinca-beta.git/
+- cmd: git remote add origin https://github.com/martin12333/PyMartinCA
+   when: 1584442271
+   
+   
+git remote add origin https://github.com/martin12333/PyMartinCA
+   git init --separate-git-dir $HOME/GIT-bk04/pymartinca-beta.git
+git remote add origin https://github.com/martin12333/PyMartinCA
+git checkout -b beta
+git checkout -b beta
+
+
+git add -vv  '*.py' '*.txt' '*.md' '*.sqc' '*.rle' '*.ipynb' '*.csv' '*.sh' '*.awk' old*.git 
+git add -vv  '*.py' '*.txt' '*.md' '*.sqc' '*.rle' '*.ipynb' '*.csv' '*.sh' '*.awk' old*.git 
+
